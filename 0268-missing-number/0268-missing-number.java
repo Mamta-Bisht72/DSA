e91d4b4b6d60bde47f562ae4edd1cb0392220464
Mamta-Bisht72 <80507735+Mamta-Bisht72@users.noreply.github.com>
2024-05-27 21:57:40 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (35.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int miss=0;
+        /*int miss=0;
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++)
         {
@@ -9,6 +9,14 @@ class Solution {
             else 
                 miss++; 
         }
-        return miss;
+        return miss;*/
+        int sum=0;
+        int n=nums.length;
+        int tsum=(n*(n+1))/2;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        return tsum-sum;
     }
 }
